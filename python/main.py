8fd37142144a6017a8e843aced9815910467d52c
alisha <alisha.tarikaa@gmail.com>
2023-10-30 23:38:47 +0530

    Delete python directory

--- python/main.py
+++
@@ -1,6 +0,0 @@
-import os
-if(not os.path.exists("data")):
- os.mkdir("Data")
-for i in range(0,100):
- os.mkdir(f"data/Day{i+1}")
-
--- python/new.py
+++
@@ -1,5 +0,0 @@
-def welcome():
-  print("my name")
-print(__name__)
-if __name__=="__main__":
- welcome()
